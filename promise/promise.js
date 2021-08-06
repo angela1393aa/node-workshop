21fb78ab256cacb55e8f464683095735b5527426
angela1393aa <angela1393aa@gmail.com>
2021-08-06 15:23:26 +0800

    add promise.js

---
+++ promise/promise.js
@@ -0,0 +1,22 @@
+let doWork = function (job, timer, isOK) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let dt = new Date();
+      if (isOK) {
+        resolve(`完成工作: ${job} at ${dt.toISOString()}`);
+      } else {
+        reject(`${job}失敗`);
+      }
+    }, timer);
+  });
+};
+//pending
+let job1 = doWork("刷牙", 3000, true);
+job1.then(
+  function (resolve) {
+    console.log("第一個被呼叫", resolve);
+  },
+  function (reject) {
+    console.log("第二個被呼叫", reject);
+  }
+);
