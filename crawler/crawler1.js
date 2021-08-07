4d35701816633eda156a1ff78a6b77a85cb26c53
angela1393aa <angela1393aa@gmail.com>
2021-08-07 16:21:53 +0800

    用 axios.get 抓到網址的資料跟增加動態日期

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -1,17 +1,15 @@
 const axios = require("axios");
 //日期
 const moment = require("moment");
+let today = moment().format("YYYYMMDD");
 axios
-  .get(
-    "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210807&stockNo=2330",
-    {
-      params: {
-        response: "json",
-        date: moment().format("YYYYMMDD"),
-        stockNo: "2330",
-      },
-    }
-  )
+  .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+    params: {
+      response: "json",
+      date: today,
+      stockNo: "2330",
+    },
+  })
   .then((response) => {
-    console.log(result.data);
+    console.log(response.data);
   });
