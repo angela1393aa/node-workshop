dc4f42a2181483cce412766fbf562ada252494d9
angela1393aa <angela1393aa@gmail.com>
2021-08-08 11:03:52 +0800

    async-await版

---
+++ crawler/crawler-fs-async-await.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs");
+
+function resultCode() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("stock.txt", "utf8", function (err, stockCode) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stockCode);
+      }
+    });
+  });
+}
+function resultData(stockCode) {
+  return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+    params: {
+      response: "json",
+      date: moment().format("YYYYMMDD"),
+      stockNo: stockCode,
+    },
+  });
+}
+
+async function getData() {
+  try {
+    let stockCode = await resultCode();
+    let result = await resultData(stockCode);
+    console.log(result.data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+getData();
