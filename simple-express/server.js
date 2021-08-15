c1adb1eccddcf1df5fc54c7931031ee198941539
angela1393aa <angela1393aa@gmail.com>
2021-08-15 11:35:59 +0800

    add

--- simple-express/server.js
+++ simple-express/server.js
@@ -3,6 +3,15 @@ const express = require("express");
 // 利用 express 建立了一個 express application
 let app = express();
 
+app.use(function (request, response, next) {
+    console.log("中間鍵");
+    next();
+  });
+app.use(function (request, response, next) {
+    console.log(req.method);
+    next();
+});
+
 // HTTP Method: get, post, put, patch, delete
 app.get("/", function (request, response, next) {
   response.send("Hello");
